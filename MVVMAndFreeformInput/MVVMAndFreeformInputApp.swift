41d117077b673d89ee553d074c29a1fe4a4836e0
Kevin Lam <klam@lcs.on.ca>
2025-02-21 11:44:40 -0500

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Kevin Lam on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
